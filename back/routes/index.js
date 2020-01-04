965d86747877de4f226b730e83ac13ee07ba8f80
ncerron <natalia.cerron@gmail.com>
2020-01-04 20:18:14 -0300

    routes

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,8 +3,8 @@ const router = express()
 const {User, Favourite} = require("../models")
 const Sequelize = require('sequelize')
 
-
-router.post('/register', (req, res) => {
+//users
+router.post('/user/register', (req, res) => {
     if (req.body.email && req.body.pass) {
       console.log(req.body)
         User.create(req.body)
@@ -26,5 +26,58 @@ router.post('/register', (req, res) => {
         res.status(400).send("Email and password are required.");
       }
 })
+
+//falta autenticacion de passport
+router.post('/user/logIn', (req,res)=>{
+  console.log("login")
+  res.status(200).send("logueado");
+})
+
+router.post('/user/logOut', (req,res)=>{
+  req.logout()
+  res.sendStatus(202)
+})
+
+router.post('/user/:userId', ()=>{
+  User.findAll({
+    where:{
+      id: req.params.userId
+    }
+  })
+  .then(users => res.json(users))
+  .catch(err=>{
+    console.log(err, "error busqueda ")
+    res.status(400).send("No se encontraron coincidencias");
+  })
+})
+
+
+//favourites
+router.post('/favourite/add', () => {
+  console.log(req.body)
+  Favourite.create(req.body)
+    .then(favourite => {
+      res.send(favourite);
+    })
+})
+
+
+
+//elimina de favoritos y luego actualiza la lista
+router.post('/favourite/delete/:favouriteId/:userId', () => {
+  Favourite.destroy({
+    where: {
+      id: req.params.favouriteId
+    }
+  })
+    .then(() => {
+      User.findAll({
+        where: {
+          id: req.params.userId
+        }
+      })
+    })
+})
+
 
 module.exports = router;
