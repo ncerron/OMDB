f1cd577d40a4f22122d4602d48d6d32a8935b04f
ncerron <natalia.cerron@gmail.com>
2020-06-11 21:10:08 -0300

    install more cors

--- back/server.js
+++ back/server.js
@@ -13,6 +13,12 @@ const app = express();
 app.use(cors())
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const port= process.env.PORT || 3000
 
 require('dotenv').config()
